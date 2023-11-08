e8033e0027e87251e71855d228cda5ec6289a685
Kishan <kishanoffcl08@gmail.com>
2023-11-08 16:59:59 +0530

    This is New

--- src/Array/LargestSubArrayZoho.java
+++ src/Array/LargestSubArrayZoho.java
@@ -3,12 +3,12 @@ package Array;
 public class LargestSubArrayZoho {
     public static void main(String[] args) {
 
-        int  arr[] = {1,0,1,1,1,0,0};
+        int[] arr = {1,0,1,1,1,0,0};
 
         int n = arr.length;
 
         for (int i = 0; i < n-2; i++) {
-            int sum = 0;
+            int sum ;
             if(arr[i]==1){
                 sum = 1;
             }
@@ -16,9 +16,12 @@ public class LargestSubArrayZoho {
                 sum = -1;
             }
             for (int j = i+1; j < n-1 ; j++) {
-                
+                sum = 4;
 
             }
+            System.out.println(sum);
+
+
 
         }
 
